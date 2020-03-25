51be6821865fd9ede22279074a01e003a0a0ec4d
rcunyat <cunyat.r@gmail.com>
2020-03-25 22:56:28 +0100

    BootcampSchema first approach

---
+++ models/Bootcamp.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const BootcampSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+    unique: true,
+    trim: true,
+    maxlength: [50, "Name can not be more than 50 characters"]
+  },
+  slug: String,
+  description: {
+    type: String,
+    required: [true, "Please add a description"],
+    maxlength: [500, "Description can not be more than 500 characters"]
+  },
+  website: {
+    type: String,
+    match: [
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+      "Please use a valid URL with http or https"
+    ]
+  },
+  phone: {
+    type: String,
+    maxlength: [20, "Phone number can nou be longer than 20 characters"]
+  },
+  email: {
+    type: String,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please enter a valid email"
+    ]
+  },
+  address: {
+    type: String,
+    required: [true, "Please add an address"]
+  },
+  location: {
+    // GEOJson point
+    type: {
+      type: String,
+      enum: ["Point"],
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+      index: "2dsphere"
+    }
+  }
+});

a6587118d56e7e4e9033946aae85cbab06a0f80f
rcunyat <cunyat.r@gmail.com>
2020-03-25 18:24:27 +0100

    Bootcamp routes setup

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
 
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+
+// Mount routes
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 

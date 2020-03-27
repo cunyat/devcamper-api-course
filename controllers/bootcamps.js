89ae8aeda06774ed4d687656a03050f068fd8fb8
rcunyat <cunyat.r@gmail.com>
2020-03-27 11:34:35 +0100

    Add geospatial query

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,6 +1,7 @@
 const Bootcamp = require("../models/Bootcamp");
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middlewares/async");
+const geocoder = require("../utils/geocoder");
 
 /**
  * @description   Get all bootcamps
@@ -78,4 +79,29 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     );
 
   res.status(200).json({ success: true, data: {} });
+});
+
+/**
+ * @description   Get bootcamps within a radius
+ * @route         GET /api/v1/bootcamp/radius/:zipcode/:distance
+ * @access        Private
+ */
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  // Get lat/long from geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
+
+  // Calc radius using radians
+  // Divide dist by radius of Earth (3,963 mi / 6,378 km)
+  const radius = distance / 3963;
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
+  });
+
+  res
+    .status(200)
+    .json({ succes: true, count: bootcamps.length, data: bootcamps });
 });
